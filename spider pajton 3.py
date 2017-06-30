9423fef515c4711c028dc100ec18fa5cf9504138
vladimir9199 <vladanita@gmail.com>
2017-06-30 16:24:04 +0200

    Add files via upload

---
+++ spider pajton 3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+recenica="Milena je sinoć videla Mitra, nežnog, zelenog skakavca ispred stacionara i uzviknula je:\"Ju, Mitre, uplašio si me!\". Ali Mitar se na to nije obazirao - žustro je skočio u Mileninom pravcu. Videvši to, Milena polete preko dvorišta i polete ka stacionaru brzinom svetlosti. Nažalost, svetlost je brža od automatskog mehanizma za otvaranje vrata. I tako ostade Milenin trag na ulaznim vratima IS Petnica."
+recenica=recenica.lower()
+lista_reci=recenica.split(" ")
+print (lista_reci)
+recenica = "Dokle ću više biti niska!"
+karakteri = list(recenica)
+print(karakteri)
