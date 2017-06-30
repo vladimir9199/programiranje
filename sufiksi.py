b8048c22fb0dba8e46f78cba31505ad30a1fff75
vladimir9199 <vladanita@gmail.com>
2017-06-30 17:02:41 +0200

    Add files via upload

---
+++ sufiksi.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+rec="sings"
+rec_osnova=rec [0:4]
+print(rec_osnova)
+rec_sufiks=rec[-1]
+print(rec_sufiks)
+
+rec="pevač"
+rec_osnova=rec [0:3]
+print(rec_osnova)
+rec_sufiks=rec[3:5]
+print(rec_sufiks)
+
+rec="beogradski"
+rec_osnova=rec [0:7]
+print(rec_osnova)
+rec_sufiks=rec[7:10]
+print(rec_sufiks)
+
+rec="spavanje"
+rec_osnova=rec [0:5]
+print(rec_osnova)
+rec_sufiks=rec[5:8]
+print(rec_sufiks)
