8a9d394b643b759ff4f5ddd47de55b2222e1c651
vladimir9199 <vladanita@gmail.com>
2017-07-01 00:09:21 +0200

    Add files via upload

---
+++ domaci 1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+priča="Živele u jednoj bari tri žabe. Svađale su se oko toga koja od njih najbolje peva, čiji je glas najlepši. Njihovoj svađi nikad ne bi došao kraj da nisu odlučile da organizuju takmičenje da se vidi ko je najbolji. To je za stanovnike bare i okoline bio veliki događaj. Sve žabe, i velike i male, su došle da svojim prisustvom pomognu odluku žirija. Ali pred sam početak svi shvatiše da nisu izabrali žiri. Mnogo njih je želelo da odlučuju koja od tri žabe najlepše peva i zato je bila takva galama da niko nikog nije čuo šta govori. Roda, patka i zmija čuju kreketanje (a taj zvuk ih je neodoljivo privlačio) i požure da vide šta se događa. Žabe, ne razmišljajući mnogo, zamole rodu, patku i zmiju da budu članovi žirija. One pristanu i takmičenje poče. Tri žabe su naizmenično pevale sve dok nisu iscrple svoj bogati repertoar i dok nisu promukle. Publika je bila oduševljena. Članovi žirija zamoliše za tišinu i rekoše takmičarkama da priđu bliže da im čestitaju i proglase pobednicu. I žabe priđoše umorne i sretne. Možemo zamisliti šta se dogodilo takmičarkama. A i publici iz prvih redova. Te večeri su roda, patka i zmija imale obilnu večeru. Žabama nije bilo do pesme. Celu noć su razmišljale i šaputale. Jedva cujno saputanje zamenilo je doskorasnji glasni kreket. Ostale su bez glasa i prijatelja. Svake godine na taj dan zabe umuknu i sa strahom se secaju se svoje skupe greske" 
+lista_rečenica=priča.split(". ")
+print(lista_rečenica)
+print(len(lista_rečenica))
+
+
